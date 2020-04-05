7a9c5a74325a7b4838d3ad5c4c4d34e1bcded1d5
descolar-ceo <emmanuellamugi@gmail.com>
2020-04-05 10:07:27 +0200

    feat(Welcome screen):Setup a welcome screen
    -Add a welcome screen
    -Add routing to WelcomeScreen in App.js file
    -Add icon of phone history store on welcome screen
    
    [Finishes #172160549]

---
+++ assets/text/eng/callLogPermissions.js
@@ -0,0 +1,1 @@
+export const CALL_LOG_PERMISSIONS = `To be able to save your call log, you must tap the button “Sync. your call log” and tap “ALLOW” in the dialog box to allow “Phone History Store” accessing your call log`
---
+++ assets/text/eng/welcomeText.js
@@ -0,0 +1,2 @@
+export const WELCOME_TEXT =
+  'Welcome to Phone History Store! Here are few steps to follow in order to work with us  perfectly. Okay Let’s go!';
